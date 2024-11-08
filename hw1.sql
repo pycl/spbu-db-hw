12dd1fc1ea2a5eef3883b4e00130fc9c88f2babd
pycl <1031069974@qq.com>
2024-11-08 22:33:35 +0300

    update

--- hw1.sql
+++ hw1.sql
@@ -10,7 +10,7 @@ CREATE TABLE Groups(
     id        VARCHAR(255) PRIMARY KEY    NOT NULL,
     full_name VARCHAR(30)                 NOT NULL,
     short_name VARCHAR(30)                 NOT NULL,
-    students_ids VARCHAR(255)             NOT null
+    students_ids VARCHAR[]             NOT null
 );
 
 CREATE TABLE Students(
@@ -18,7 +18,7 @@ CREATE TABLE Students(
     first_name  VARCHAR(30)               NOT NULL,
     last_name   VARCHAR(30)               NOT NULL,
     group_id    VARCHAR(255)              NOT NULL,
-    courses_ids VARCHAR(255)              NOT NULL,
+    courses_ids VARCHAR[]              NOT NULL,
     FOREIGN KEY (group_id) REFERENCES groups(id)  
 );
 
@@ -36,17 +36,17 @@ INSERT INTO Courses(id,name,is_exam,min_grade,max_grade) VALUES
 (4, 'Информатика', TRUE, 81, 98); 
 
 INSERT INTO groups(id,full_name,short_name,students_ids) VALUES
-(1, 'Группа 1', 'г1', 'st1,st2'),  
-(2, 'Группа 2', 'г2', 'st3,st4'),
-(3, 'Группа 3', 'г3', 'st5,st6');  
+('1', 'Группа 1', 'г1', ARRAY['st1','st2']),  
+('2', 'Группа 2', 'г2', ARRAY['st3','st4']),
+('3', 'Группа 3', 'г3', ARRAY['st5','st6']);  
 
 INSERT INTO Students(id,first_name,last_name,group_id,courses_ids) VALUES
-('st1', 'Иван', 'Иванов', 1, '1,2'),  
-('st2', 'Пётр', 'Петров', 1, '1,3'),  
-('st3', 'Владимир', 'Жуков', 1, '1,4'),
-('st4', 'Александр', 'Смирнов', 2, '2,3'),
-('st5', 'Николай', 'Белоусов', 3, '2,4'), 
-('st6', 'Даяна', 'Кириллова', 3, '3,4');
+('st1', 'Иван', 'Иванов', '1', ARRAY['1','2']),  
+('st2', 'Пётр', 'Петров', '1', ARRAY['1','3']),  
+('st3', 'Владимир', 'Жуков', '1', ARRAY['1','4']),
+('st4', 'Александр', 'Смирнов', '2', ARRAY['2','3']),
+('st5', 'Николай', 'Белоусов', '3', ARRAY['2','4']), 
+('st6', 'Даяна', 'Кириллова', '3', ARRAY['3','4']);
 
 INSERT INTO Math(students_id,grade,grade_str) VALUES
 ('st1', 65, 'min65-max89'),
@@ -59,6 +59,7 @@ FROM Students s
 JOIN Math m ON s.id = m.students_id
 JOIN Courses c ON name = 'Math'
 WHERE m.grade > c.min_grade;
+LIMIT 3;
 
 -- Агрегация: вычислить средний балл по математике 
 SELECT c.min_grade, c.max_grade, (SELECT AVG(grade) FROM Math) AS average_grade  
